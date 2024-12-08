dc1615e635ca8eb7a11436d7d7caf84a1fa12a37
NatalieGuseva <gusberd81@gmail.com>
2024-12-08 20:45:42 +0500

    Task Executed

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,74 @@
+package hiber.model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String model;
+
+    @Column
+    private int series;
+
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "car")
+    @JoinColumn(name = "userId")
+    private User user;
+
+
+    public Car() {
+
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", series=" + series +
+                '}';
+    }
+}
